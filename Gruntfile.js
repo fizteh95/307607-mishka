b314179aba0147ee1917f333032a8c9d6976b5d1
fizteh <causeLove95@yandex.ru>
2018-07-22 13:38:04 +0300

    Начинаю верстку

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,192 @@
-module.exports = function(grunt) {
-	grunt.loadNpmTasks("grunt-contrib-less");
+"use strict";
 
-	grunt.initConfig({
-		less: {
-			style: {
-				files: {
-					"css/style.css": "less/style.less"
-				}
-			}
-		}
-	});
+module.exports = function(grunt) {
+
+  require("load-grunt-tasks")(grunt);
+
+  grunt.initConfig({
+    less: {
+      style: {
+        files: {
+          "source/css/style.css": "source/less/style.less"
+        }
+      }
+    },
+
+    postcss: {
+      style: {
+        options: {
+          processors: [
+            require("autoprefixer")()
+          ]
+        },
+        src: "source/css/*.css"
+      }
+    },
+
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: [
+            "build/*.html",
+            "build/css/*.css"
+          ]
+        },
+        options: {
+          server: "build/",
+          watchTask: true,
+          notify: false,
+          open: true,
+          cors: true,
+          ui: false
+        }
+      }
+    },
+
+    watch: {
+      html: {
+        files: ["source/*.html"],
+        tasks: ["posthtml"]
+      },
+      style: {
+        files: ["source/less/**/*.less"],
+        tasks: ["less", "postcss","csso"]
+      },
+      scripts: {
+        files: ["source/js/**/*.js"],
+        tasks: ["copy:watch","uglify"],
+        options: {
+          spawn: false,
+        }
+      }
+    },
+
+    csso: {
+      style: {
+        options: {
+          report: "gzip"
+        },
+        files: {
+          "build/css/style.min.css": ["source/css/style.css"],
+        }
+      }
+    },
+
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3,
+          progressive: true
+        },
+        files: [{
+          expand: true,
+          src: ["source/img/**/*.{png,jpg,svg}"]
+        }]
+      }
+    },
+
+    cwebp: {
+      images: {
+        options: {
+          q: 90
+        },
+        files: [{
+          expand: true,
+          src: ["source/img/**/*.{png,jpg}"]
+        }]
+      }
+    },
+
+    svgstore: {
+      options: {
+        includeTitleElement: false
+      },
+      sprite: {
+        files: {
+          "build/img/sprite.svg" : ["source/img/icon-*.svg"]
+        }
+      }
+    },
+
+    posthtml: {
+      options: {
+        use: [
+          require("posthtml-include")()
+        ]
+      },
+      build: {
+        files: [{
+          expand: true,
+          cwd: "source",
+          src: ["*.html"],
+          dest: "build"
+        }]
+      }
+    },
+
+    clean: {
+      build: ["build"]
+    },
+
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "source",
+          src: [
+            "fonts/**/*.{woff,woff2}",
+            "img/**",
+            "js/**"
+          ],
+          dest: "build"
+        }]
+      },
+      watch: {
+        files: [{
+          expand: true,
+          cwd: "source",
+          src: [
+            "js/**"
+          ],
+          dest: "build"
+        }]
+      }
+    },
+
+    htmlmin: {                                     // Task
+      dist: {                                      // Target
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {                                   // Dictionary of files
+          "build/index.html": "build/index.html",     // "destination": "source"
+          "build/form.html": "build/form.html",
+          "build/photo.html": "build/photo.html"
+        }
+      }
+    },
+
+    uglify: {
+      my_target: {
+        files: {
+          "build/js/menu.min.js": ["build/js/menu.js"]
+        }
+      }
+    }
+  });
+
+  grunt.registerTask("serve", ["browserSync", "watch"]);
+
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "less",
+    "postcss",
+    "csso",
+    "svgstore",
+    "posthtml",
+    "htmlmin",
+    "uglify"
+  ]);
 };
